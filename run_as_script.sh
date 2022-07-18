00557e04fd3b2d9e85bdc44cf4ebe862b61e3149
Aisu Wata <aisu.wata0@gmail.com>
2022-07-18 19:24:01 -0300

    run_as_script.sh extra explanation line

--- run_as_script.sh
+++ run_as_script.sh
@@ -1,6 +1,6 @@
-
 echo "This should be executed in PmxEditor's folder with PmxEditorPreviewGen.dll installed in its plugins folder"
 # Example:
+# You have `PmxEditor/`` and this project's folder, `PmxPreviews/``, beside each other.
 # copy this file to PmxEditor's folder
 # open a terminal in that PmxEditor's folder
 # run: . run_as_script.sh "M:\MMD\data\models\characters"
